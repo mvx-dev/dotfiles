11fbce0498d9d5166baa79c2129355cf3c62e121
mvx-dev <oskardmeszaros@protonmail.com>
2025-07-08 11:22:14 +0000

    feat: updated nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,18 @@
-require("config")
+--rose-pine call nvim settings
+require("config.settings")
+
+-- call lazy (plugin manager)
+require("config.lazy")
+
+-- call custom keybinds for plugins
+require("config.keybinds")
+
+-- call lsp and related settings
+require("config.lsp")
+
+-- call colourschemehe
+vim.cmd([[colorscheme catppuccin]])
+
+require("notify").setup({
+    background_colour = "#000000",
+})
---
+++ nvim/.config/nvim/lua/config/settings.lua
@@ -0,0 +1,39 @@
+-- leader key
+vim.g.mapleader = " "
+
+-- disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- indent
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.tabstop = 4
+
+-- wrapping
+vim.opt.shiftround = true
+vim.opt.wrap = true
+vim.opt.signcolumn = 'yes'
+
+-- numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.ruler = true
+
+-- search
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+
+-- errors
+vim.opt.visualbell = false
+
+-- set cursor to underline
+vim.cmd [[
+    set guicursor=a:hor20
+]]
+
+vim.opt.colorcolumn = "80"
