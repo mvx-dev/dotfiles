2f440dc6fa59f0fa8ea4ee2bce2a09d03668c7d3
mvx-dev <122265223+mvx-dev@users.noreply.github.com>
2025-12-21 17:12:58 +1100

    feat: Updated fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,14 +2,19 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 set fish_greeting
+
 alias ls "eza -alo --git --group-directories-first"
+alias nv "bob run nightly"
+pyenv init - fish | source
 zoxide init --cmd cd fish | source
 
+set GPG_TTY $(tty)
+
 function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
 end
