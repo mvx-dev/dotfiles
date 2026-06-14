d2f2e1569cdbf57467e9823304ef8a9e2c941138
mvx-dev <aspen_davisGAED@protonmail.com>
2026-06-14 14:00:32 +1000

    feat: updated harpoon config

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -35,6 +35,36 @@ local function toggle_telescope(harpoon_files)
 		:find()
 end
 
+local make_finder = function()
+	local paths = {}
+	for _, item in ipairs(harpoon:list()) do
+		table.insert(paths, item.value)
+	end
+
+	return require("telescope.finders").new_table({
+		results = paths,
+	})
+end
+
+require("telescope.pickers").new({}, {
+	prompt_title = "Harpoon",
+	finder = make_finder(),
+	previewer = conf.file_previewer({}),
+	sorter = conf.generic_sorter({}),
+	attach_mappings = function(prompt_buffer_number, map)
+		map("i", "<C-d>", function()
+			local state = require("telescope.actions.state")
+			local selected_entry = state.get_selected_entry()
+			local current_picker = state.get_current_picker(prompt_buffer_number)
+
+			harpoon:list():removeAt(selected_entry.index)
+			current_picker:refresh(make_finder())
+		end)
+
+		return true
+	end,
+})
+
 -- Keybinds
 vim.keymap.set("n", "<leader>a", function()
 	harpoon:list():add()
