835596e6e6de5ac5d3f879177fb01e62e040e1b0
Oskar Meszaros <oskardmeszaros@protonmail.com>
2026-07-13 09:59:59 +1000

    feat: changed to latexmk for better bibliography support

--- nvim/.config/nvim/lua/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -1,5 +1,5 @@
 vim.pack.add({ "https://github.com/lervag/vimtex" })
-vim.g.vimtex_compiler_method = "generic"
+vim.g.vimtex_compiler_method = "latexmk"
 vim.g.vimtex_compiler_generic = {
 	command = "ls *.tex | entr -n -c tectonic /_ --synctex --keep-logs",
 }
