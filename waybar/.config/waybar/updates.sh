364ff9df16fa031a7aa6bf5528634e5cb3c353e0
Oskar Meszaros <oskardmeszaros@protonmail.com>
2026-07-16 20:28:19 +1000

    feat: updated update checker

--- waybar/.config/waybar/updates.sh
+++ waybar/.config/waybar/updates.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 count=$(checkupdates 2>/dev/null | wc -l)
 
-if [[ "$count" -gt 10 ]]; then
+if [[ "$count" -gt 20 ]]; then
     echo "{\"text\":\"󰅢\",\"tooltip\":\"$count updates available\"}"
 fi
